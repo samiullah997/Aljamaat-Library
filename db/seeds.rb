20134c0fd5b1ba244d606770808c6549010af41f
samiullah997 <samiullahk997@gmail.com>
2022-12-26 23:34:47 +0500

    Insert record for user, category and post

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# user = User.create(first_name: "John", last_name: "Doe", address: "123 Main St", phone_number: "123-456-7890", role: "admin")
-# category = Category.create(title: "Test Category", user_id: user.id)
-# post = Post.create(title: "Test Post", desc: "This is a test post", user_id: 1, category_id: 1)
+user = User.create(first_name: "John", last_name: "Doe", address: "123 Main St", phone_number: "123-456-7890", role: "admin")
+category = Category.create(title: "Test Category", user_id: user.id)
+post = Post.create(title: "Test Post", desc: "This is a test post", user_id: 1, category_id: 1)
