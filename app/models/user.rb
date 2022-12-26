0a3d1b03485515914e7d916c03f1342b34c1e32d
samiullah997 <samiullahk997@gmail.com>
2022-12-26 23:40:10 +0500

    Fix rubocop erros

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    has_many :categories, dependent: :destroy
-    has_many :posts, through: :categories, dependent: :destroy
+  has_many :categories, dependent: :destroy
+  has_many :posts, through: :categories, dependent: :destroy
 end
