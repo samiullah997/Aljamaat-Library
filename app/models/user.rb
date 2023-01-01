9a544e44d4daaad13c6aa2f3e003b10bb95a8c0c
samiullah997 <samiullahk997@gmail.com>
2023-01-01 23:08:03 +0500

    update user, category, and post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true, length: { minimum: 3, maximum: 25 }
+  validates :last_name, presence: true, length: { minimum: 3, maximum: 25 }
+  validates :address, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :role, presence: true, length: { minimum: 3, maximum: 25 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
